cbc9b0657ca78c975de9e134ca78a3eaae637843
ashb155 <b.ashutosh198@gmail.com>
2026-09-10 15:35:32 +0530

    cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -213,7 +213,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.ui:ui")
     detektPlugins("io.nlopez.compose.rules:detekt:0.4.17")
     lintChecks("com.slack.lint.compose:compose-lint-checks:1.4.2")
 
